0f84750ca66f9bf463a7661dd67ce3dd57a59b8f
David Swanson <54049277+mushBrainDave@users.noreply.github.com>
2026-06-04 00:07:39 -0500

    tailscale on mqtt server

--- cla/lib/config.dart
+++ cla/lib/config.dart
@@ -1,6 +1,6 @@
 import 'package:pocketbase/pocketbase.dart';
 
-const String mqttHost = '192.168.0.49';
+const String mqttHost = 'mqtt.tailb99a87.ts.net';
 const String topicPrefix = 'home/foodbowl';
 const String pbUrl = 'https://ubuntuserver.tailb99a87.ts.net';
 
--- cla/lib/mqtt_client_factory_web.dart
+++ cla/lib/mqtt_client_factory_web.dart
@@ -2,5 +2,5 @@ import 'package:mqtt_client/mqtt_browser_client.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 
 MqttClient createMqttClient(String host, String clientId) {
-  return MqttBrowserClient.withPort('ws://$host', clientId, 9001);
+  return MqttBrowserClient.withPort('wss://$host', clientId, 9001);
 }
