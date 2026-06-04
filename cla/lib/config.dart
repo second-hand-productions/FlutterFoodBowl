a81aa936a5635b6370cd0071997c9842227d6860
David Swanson <54049277+mushBrainDave@users.noreply.github.com>
2026-06-03 20:18:37 -0500

    install tailscale for remote access

--- cla/lib/config.dart
+++ cla/lib/config.dart
@@ -2,6 +2,6 @@ import 'package:pocketbase/pocketbase.dart';
 
 const String mqttHost = '192.168.0.49';
 const String topicPrefix = 'home/foodbowl';
-const String pbUrl = 'http://pocketbase.lan';
+const String pbUrl = 'https://ubuntuserver.tailb99a87.ts.net';
 
 final pb = PocketBase(pbUrl);
